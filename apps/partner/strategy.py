7136b1b9139ee0d8177adc7dd178356fcc1921ed
JK <jerinisready@gmail.com>
2020-07-09 18:13:09 +0530

    grocery_products dummy data generator

--- apps/partner/strategy.py
+++ apps/partner/strategy.py
@@ -8,7 +8,7 @@ class Selector(object):
     Custom selector to return a Indian-specific strategy that charges GST
     """
     def strategy(self, request=None, user=None, **kwargs):
-        if request and request.session.get('pincode'):
-            return IndianStrategyUsingPincode(request=request, user=user, **kwargs)
+        # if request and request.session.get('pincode'):
+        #     return IndianStrategyUsingPincode(request=request, user=user, **kwargs)
         return MinimumValueStrategy()
 
