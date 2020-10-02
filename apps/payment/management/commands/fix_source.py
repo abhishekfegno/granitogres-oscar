7048737552bcdc86b4d3280f1c4a94e10b8d6e53
JK <jerinisready@gmail.com>
2020-10-02 10:20:43 +0530

    checkout management command

--- apps/payment/management/commands/fix_source.py
+++ apps/payment/management/commands/fix_source.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from django.core.management.base import BaseCommand
 
 from apps.payment.models import Source
@@ -12,12 +14,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         fixed = 0
         total = 0
-
-        for source in Source.all.all():
+        sources_with_same_rate = defaultdict(list)
+        for source in Source.all.filter():
             total += 1
             if source.order.total_incl_tax != source.amount_allocated:
                 fixed += 1
                 source.is_active = False
                 source.save()
                 print(str(source), "Fixed!")
+            else:
+                sources_with_same_rate[source.order].append(source)
+        for order, sources in sources_with_same_rate.items():
+            master = sources and sources[:1]
+            slaves = sources[1:]
+            for slave in slaves:
+                slave.is_active = False
+                slave.save()
+
         print(f"Fixed {fixed} out of {total} issues!")
