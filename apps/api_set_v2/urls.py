cf662c0fefdfaa3ece58b2f0e5387960898e3b4e
JK <jerinisready@gmail.com>
2021-03-26 15:32:52 +0530

    merged all apis in v1 to v2

--- apps/api_set_v2/urls.py
+++ apps/api_set_v2/urls.py
@@ -1,19 +1,10 @@
-from django.conf.urls import url
+from apps.api_set.urls import *
 from django.urls import path, include
 # Loading V1 Apis In order to patch
 from apps.api_set.views.catalogue import product_suggestions
 from apps.api_set.views.index import (
     home, offer_products, offers
 )
-# from apps.api_set.views.orders import orders, orders_detail, orders_more_detail, order_line_return_request, orders_v2
-# from apps.api_set.views.product_listing_query_based import product_list
-
-# from apps.api_set.views.public import availability, return_reasons_list
-# from apps.api_set.views.wishlist import wish_list
-
-# from apps.api_set.views.auth import (
-#     SendOTP, resend_otp, LoginWithOTP,
-# )
 #  End loading v1 apis
 from apps.api_set_v2.views.catalogue import product_detail_web
 
@@ -23,9 +14,9 @@ from apps.api_set_v2.views.orders import orders
 from apps.api_set_v2.views.product_listing_query_based import product_list
 
 v1__registration_apis = [
-    # path('send-otp/', SendOTP.as_view(), name="api-v1--send-otp"),
-    # path('resend-otp/', resend_otp, name="api-v1--resend-otp"),
-    # path('login-with-otp/', LoginWithOTP.as_view(), name="api-v1--login-otp"),
+    path('send-otp/', SendOTP.as_view(), name="api-v1--send-otp"),
+    path('resend-otp/', resend_otp, name="api-v1--resend-otp"),
+    path('login-with-otp/', LoginWithOTP.as_view(), name="api-v1--login-otp"),
     # path('update-profile/', UpdateProfile.as_view(), name="api-v1--update-profile"),
 ]
 
@@ -36,28 +27,28 @@ home_urlpatterns = [
     # DEPRECATED path("offers/<int:pk>/", offer_products, name="api-offer-products-v2"),       #! instead use product_list api!
     path("_orders/", orders, name="api-orders-v2"),
     path("_orders/<int:pk>/", orders_detail, name="api-orders-detail-v2"),
-    # path("_orders/<int:pk>/more/", orders_more_detail, name="api-orders-more"),
-    # path("_orders/<int:pk>/return-request/", order_line_return_request, name="order_line_return_request"),
-    # path("auth/", include(v1__registration_apis)),
+    path("_orders/<int:pk>/more/", orders_more_detail, name="api-orders-more"),
+    path("_orders/<int:pk>/return-request/", order_line_return_request, name="order_line_return_request"),
+    path("auth/", include(v1__registration_apis)),
 ]
 
 account_urlpatterns = [
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    # path('rest-auth/', include('rest_auth.urls')),
-    # path('account/', include('allauth.urls')),
-    # url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email,
-    #     name='account_confirm_email'),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('account/', include('allauth.urls')),
+    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email,
+        name='account_confirm_email'),
 ]
 
 
 catalogue_urlpatterns = [
     path("catalogue/", include([
-            # path("c/", categories_list_cached, name="wnc-categories-list"),                               # category
+            path("c/", categories_list_cached, name="wnc-categories-list"),                               # category
             path("c/all/", product_list, name="wnc-all-product-list-v2"),                                   # category
             # path("c/all/new/", product_list_new, name="wnc-all-product-list-new"),                        # category
         path("c/<slug:category>/", product_list, name="wnc-category-product-list-v2"),                      # category
         path("d/<slug:product>/", product_detail_web, name="wnc-category-product-detail-web-v2"),           # detail
-        # path("f/<slug:pk>/", filter_options, name="wnc-filter-options"),                                  # filter
+        path("f/<slug:pk>/", filter_options, name="wnc-filter-options"),                                  # filter
         path("suggestions/", product_suggestions, name="wnc-product-suggestions"),                          # category
         # path("mob/", include([]))
     ]))
@@ -66,24 +57,24 @@ catalogue_urlpatterns = [
 
 basket_urlpatterns = [
     path("_basket/", include([
-        # path("", get_basket, name="wnc-get-basket"),
-        # path("wish-list/", wish_list, name="wnc-wish-list"),
-        # path("budget-bag/", budget_bag, name="wnc-budget_bag"),
-        # path("add/", product_list, name="wnc-all-product-list"),
+        path("", get_basket, name="wnc-get-basket"),
+        path("wish-list/", wish_list, name="wnc-wish-list"),
+        path("budget-bag/", budget_bag, name="wnc-budget_bag"),
+        path("add/", product_list, name="wnc-all-product-list"),
     ]))
 ]
 
 wish_list_urlpatterns = [
     path("_wishlist/", include([
-        # path("", wish_list, name="wnc-wish-list"),
-        # path("mob/", include([
-        # ]))
+        path("", wish_list, name="wnc-wish-list"),
+        path("mob/", include([
+        ]))
     ]))
 ]
 
 public_apis = [
-    # path("check-availability/<int:product>@<int:pincode>/", availability, name="api-availability"),
-    # path("return-reasons-list/", return_reasons_list, name="return-reasons-list"),
+    path("check-availability/<int:product>@<int:pincode>/", availability, name="api-availability"),
+    path("return-reasons-list/", return_reasons_list, name="return-reasons-list"),
 
 ]
 
