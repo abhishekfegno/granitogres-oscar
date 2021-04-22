e8bf056c36919ef80f7c3064027392c4fc4a15fd
jerinisready <jerinisready@gmail.com>
2021-04-22 16:16:04 +0530

    some data

--- apps/address/models.py
+++ apps/address/models.py
@@ -14,6 +14,13 @@ class UserAddress(AbstractUserAddress):
     location = PointField(null=True)
     address_type = models.CharField(max_length=12, null=True, blank=True, choices=ADDRESS_TYPE_CHOICES, default='Home')
 
-
+    @property
+    def location_data(self):
+        if self.location:
+            return {
+                'latitude': self.location.x,
+                'longitude': self.location.y,
+            }
+        return {}
 
 from oscar.apps.address.models import *     # noqa isort:skip
