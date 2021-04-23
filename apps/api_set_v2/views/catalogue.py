5de1e86995bef804d030d79a639046ab02404411
jerinisready <jerinisready@gmail.com>
2021-04-23 08:13:33 +0530

    added images

--- apps/api_set_v2/views/catalogue.py
+++ apps/api_set_v2/views/catalogue.py
@@ -12,11 +12,12 @@ from apps.api_set_v2.utils.product import get_optimized_product_dict
 from apps.catalogue.models import Product
 
 # API_V2
+from apps.utils import image_not_found
 
 
 @api_view()
 def product_detail_web(request, product: Product): # needs parent product
-    queryset = Product.browsable.all().select_related('parent')
+    queryset = Product.browsable.all().select_related('parent', ).prefetch_related('images', 'parent__images')
     serializer_class = ProductDetailWebSerializer       # v2
     product = get_object_or_404(queryset, pk=product)
     if product.is_child:
@@ -33,10 +34,24 @@ def product_detail_web(request, product: Product): # needs parent product
     for r in response:
         response = r
         break
+
+    def get_primary_image(instance):
+        if instance.is_child:
+            return None
+        request = (self.context or {}).get('request', empty())  # noqa: mixin assured
+        req = request.build_absolute_uri
+        img = instance.primary_image()
+        img_mob = img['original'] if type(img) is dict else img.thumbnail_mobile_listing
+        return {
+            # 'web': req.build_absolute_uri(img_web),
+            'mobile': req(img_mob or image_not_found()),
+        }
+
     response = {
         **response,
         "url": reverse('product-detail', request=request, kwargs={'pk': product.id}),
         "description": product.description,
+        'images': [request.build_absolute_uri(u.thumbnail_mobile_listing or image_not_found()) for u in product.get_all_images()],
         "recommended_products": [a for a in get_optimized_product_dict(
             request=request,
             qs=sol,
