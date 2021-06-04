a7faff5f47066973d8ca75d2896c012c4a0e4859
jerinisready <jerinisready@gmail.com>
2021-06-04 11:33:18 +0530

    push settings

--- osc/config/push_notifications.py
+++ osc/config/push_notifications.py
@@ -4,7 +4,6 @@ import os
 from osc.config.base_dir import BASE_DIR
 
 
-
 PUSH_NOTIFICATIONS_SETTINGS = {
         # 'FCM_API_KEY': os.environ.get('FCM_API_KEY', ),
         # 'GCM_API_KEY': None,
@@ -20,6 +19,12 @@ PUSH_NOTIFICATIONS_SETTINGS = {
 
                         # required FCM setting
                         "API_KEY": os.environ.get('FCM_API_KEY', ),
+                },
+                "GROCERY_IOS": {
+                        # PLATFORM (required) determines what additional settings are required.
+                        "PLATFORM": "APNS",
+                        # required APNS setting
+                        "CERTIFICATE": os.path.join(BASE_DIR, 'keys', 'cert.pem'),
                 },
                 "DELIVERYBOY": {
                         # PLATFORM (required) determines what additional settings are required.
