0ab03445f280dfc024bb5d3b1a7e375c479706b2
jerinisready <jerinisready@gmail.com>
2021-06-09 10:29:28 +0530

    asdf

--- apps/mod_oscarapi/management/commands/dpush.py
+++ apps/mod_oscarapi/management/commands/dpush.py
@@ -8,12 +8,12 @@ from apps.users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        u = User.objects.get(pk=1)
+        u = User.objects.all()
         title = 'Grocery App Push Notification Testing!'
         message = 'Some Long Message'
         from apps.utils.pushnotifications import PushNotification
         try:
-            PushNotification(u).send_message(title, message)
+            PushNotification(*u).send_message(title, message)
         except HTTPError as err:
             print(err)
             print(err.msg)
