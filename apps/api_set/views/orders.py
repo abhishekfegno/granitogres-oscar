7efc546dfb855d0e4b2a3cf18fbf3da5ec013b58
jerinisready <jerinisready@gmail.com>
2021-06-16 16:00:13 +0530

    asdf

--- apps/api_set/views/orders.py
+++ apps/api_set/views/orders.py
@@ -78,7 +78,7 @@ def orders_detail(request, *a, **k):
 @api_view(("GET",))
 @_login_required
 def orders_more_detail(request, *a, **k):
-    _object = get_object_or_404(Order.objects.filter().prefetch_related(
+    _object = get_object_or_404(Order.objects.filter(user=request.user).prefetch_related(
         'lines',
     ).select_related(
         'billing_address', 'shipping_address'
