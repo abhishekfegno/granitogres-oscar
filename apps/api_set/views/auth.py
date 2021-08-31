65a5f9b8b2ebde56b3e4001fcbe94fc1fecf5242
abhishekfegno <89522513+abhishekfegno@users.noreply.github.com>
2021-08-31 11:15:14 +0530

    Update auth.py

--- apps/api_set/views/auth.py
+++ apps/api_set/views/auth.py
@@ -83,7 +83,7 @@ class LoginWithOTP(APIView):
     serializer_class = OtpSerializer
 
     def post(self, request, *args, **kwargs):
-        out = {'error': None, 'user': None, 'basket': None, 'cart_item_count': 0, 'new_user': None}
+        out = {'error': None, 'user': None, 'basket': None, 'cart_item_count': 0}
 
         # Validating Data
         otp_serializer = self.serializer_class(data=request.data, context={'request': request})
@@ -93,11 +93,9 @@ class LoginWithOTP(APIView):
 
         # Generating User or Respond with error
         otp_object = otp_serializer.validated_data['object']
-        out['new_user'] = False
         if not otp_object.user:  # signup
             try:
                 otp_object.generate_user()
-                out['new_user'] = True
             except Exception as e:
                 out['error'] = {
                     'non_field_errors': [str(e)]
