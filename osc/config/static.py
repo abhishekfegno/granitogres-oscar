a898005c7129a071d03c49e9b87bf9e235bf7287
JK <jerinisready@gmail.com>
2021-10-04 11:31:18 +0530

    okey to have checkout

--- osc/config/static.py
+++ osc/config/static.py
@@ -8,11 +8,11 @@ STATIC_URL = '/assets/'
 MEDIA_URL = '/src/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
-STATIC_DIR_MASTER = os.path.join(BASE_DIR, "public/assets")
+STATIC_ROOT = os.path.join(BASE_DIR, "public", "static")
+STATIC_DIR_MASTER = os.path.join(BASE_DIR, "public", "assets")
 STATICFILES_DIRS = [STATIC_DIR_MASTER, ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
 
-# INDEX_STATIC_ROOT = os.path.join(BASE_DIR, "public/react_index_page/build/static")
+# INDEX_STATIC_ROOT = os.path.join(BASE_DIR, "public/reactAPI Supports multiple payments integration, though our system doesnot' well support this feature. (futurestic one.)_index_page/build/static")
 #                                                           # special static root for index page build
 # INDEX_STATIC_URL = os.path.join(BASE_DIR, "/static/")     # special static root for index page build
