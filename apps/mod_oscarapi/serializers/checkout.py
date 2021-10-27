2c7317897466474d58fe3c7e2036ac9e791ae59f
JK <jerinisready@gmail.com>
2021-10-27 17:31:56 +0530

    reciever fixed

--- apps/mod_oscarapi/serializers/checkout.py
+++ apps/mod_oscarapi/serializers/checkout.py
@@ -105,7 +105,17 @@ class UserAddressSerializer(CoreUserAddressSerializer):
 
     def create(self, validated_data):
         point = validated_data.pop('location') if 'location' in validated_data else None
-        validated_data['country'] = Country.objects.get_or_create(iso_3166_1_a2="IN")[0]
+        india, _ = Country.objects.get_or_create(
+            iso_3166_1_a2='IN',
+            defaults={
+                'iso_3166_1_a3': 'IND',
+                'iso_3166_1_numeric': '356',
+                'printable_name': 'India',
+                'name': 'India',
+                'display_order': 0,
+                'is_shipping_country': True
+            })
+        validated_data['country'] = india
         instance = super(UserAddressSerializer, self).create(validated_data)
         if point and 'point' in point:
             instance.location = point['point']
