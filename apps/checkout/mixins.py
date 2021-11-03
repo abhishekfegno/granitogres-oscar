9c9897c7b526f7afe2a806ba2cf2305ffe28c2c5
Abhishek <=>
2021-11-03 10:48:04 +0530

    email dispatcher

--- apps/checkout/mixins.py
+++ apps/checkout/mixins.py
@@ -1,15 +1,15 @@
-from oscar.apps.checkout.mixins import OrderPlacementMixin as CoreOrderPlacementMixin
-
-
-class OrderPlacementMixin(CoreOrderPlacementMixin):
-
-    def get_message_context(self, order, code=None):
-        super().get_message_context(order, code)
-        ctx = {
-                    'orderID': order.id,
-                    'shipping_address': order.shipping_address,
-                    'date_of_order': order.date_placed,
-                    'products': order.lines.all,
-                    'total': order.total_incl_tax,
-                }
-        return ctx
+# from oscar.apps.checkout.mixins import OrderPlacementMixin as CoreOrderPlacementMixin
+#
+#
+# class OrderPlacementMixin(CoreOrderPlacementMixin):
+#
+#     def get_message_context(self, order, code=None):
+#         super().get_message_context(order, code)
+#         ctx = {
+#                     'orderID': order.id,
+#                     'shipping_address': order.shipping_address,
+#                     'date_of_order': order.date_placed,
+#                     'products': order.lines.all,
+#                     'total': order.total_incl_tax,
+#                 }
+#         return ctx
