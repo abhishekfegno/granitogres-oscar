2916830fbad45de7de44d802f0353092e3cfd77d
Abhishek <=>
2021-11-03 16:05:37 +0530

    email get_or_render

--- apps/order/processing.py
+++ apps/order/processing.py
@@ -79,7 +79,9 @@ class EventHandler(processing.EventHandler):
             files for each status to work with this logic.
             """
             order_code = f"order__{new_status.lower().replace(' ', '_')}"
-            OrderPlacementMixin().send_confirmation_message(order, order_code)
+            opm = OrderPlacementMixin()
+            opm.kwargs['request'] =setattr(self, 'request', None)
+            opm.send_confirmation_message(order, order_code)
 
         if hasattr(order, 'consignmentdelivery'):
             if new_status == settings.ORDER_STATUS_DELIVERED:
