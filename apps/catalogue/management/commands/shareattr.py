f38c630c55dc46be8fc323f2ea65c053a6ce0adc
JK <jerinisready@gmail.com>
2021-12-02 00:28:27 +0530

    asdf

--- apps/catalogue/management/commands/shareattr.py
+++ apps/catalogue/management/commands/shareattr.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
                 for attr in pc.attributes.all().values_list('code', flat=True):
                     if attr.endswith('_old') or attr == 'is_present': continue
                     print(c, hasattr(p.attr, attr) and getattr(p.attr, attr))
-                    if hasattr(p.attr, attr) and getattr(p.attr, attr) and not hasattr(p.attr, attr) or not getattr(c.attr, attr):
+                    if hasattr(p.attr, attr) and getattr(p.attr, attr) and hasattr(p.attr, attr) or not getattr(c.attr, attr):
                         val = getattr(p.attr, attr)
                         setattr(c.attr, attr, val)
                 c.attr.save()
