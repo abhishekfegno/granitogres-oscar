4e51ff418b39db5d6df2b9d75a0d44b5a4eaa37d
JK <jerinisready@gmail.com>
2021-12-02 00:30:35 +0530

    asdf

--- apps/catalogue/management/commands/shareattr.py
+++ apps/catalogue/management/commands/shareattr.py
@@ -1,5 +1,5 @@
 from django.core.management import BaseCommand
-from apps.catalogue.models import Product
+from apps.catalogue.models import Product, ProductAttribute
 
 
 class Command(BaseCommand):
@@ -12,9 +12,12 @@ class Command(BaseCommand):
                 for attr in pc.attributes.all().values_list('code', flat=True):
                     if attr.endswith('_old') or attr == 'is_present': continue
                     print(c, hasattr(p.attr, attr) and getattr(p.attr, attr))
-                    if hasattr(p.attr, attr) and getattr(p.attr, attr) and hasattr(p.attr, attr) or not getattr(c.attr, attr):
-                        val = getattr(p.attr, attr)
-                        setattr(c.attr, attr, val)
+                    if hasattr(p.attr, attr) and getattr(p.attr, attr):
+                        if not hasattr(p.attr, attr):
+                            ProductAttribute.objects.create(code=attr, name=attr, product_class=p.get_product_class())
+                        if hasattr(p.attr, attr) or not getattr(c.attr, attr):
+                            val = getattr(p.attr, attr)
+                            setattr(c.attr, attr, val)
                 c.attr.save()
 
 
