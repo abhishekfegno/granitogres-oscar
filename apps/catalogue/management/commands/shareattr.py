b8d361484af2c8b6bc5756cb65b2377893442579
JK <jerinisready@gmail.com>
2021-12-02 00:22:11 +0530

    shareattr

---
+++ apps/catalogue/management/commands/shareattr.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+from apps.catalogue.models import Product
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        for p in Product.objects.filter(structure="parent").prefetch_related('children', 'product_class'):
+            pc = p.get_product_class()
+            for c in p.children.all():
+                for attr in pc.attributes.all().values_list('code', flat=True):
+                    print(c, hasattr(p.attr, attr) and getattr(p.attr, attr))
+                    if hasattr(p.attr, attr) and getattr(p.attr, attr) and not hasattr(p.attr, attr) or not getattr(c.attr, attr):
+                        val = getattr(p.attr, attr)
+                        setattr(c.attr, attr, val)
+                c.attr.save()
+
+
+
