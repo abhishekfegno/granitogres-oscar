8678eaa8ff04f03bba3bc33c2c9c4a5adf447338
jerinisready <jerinisready@gmail.com>
2022-03-17 16:12:18 +0530

    line ricing

--- apps/api_set_v2/views/new_product_pagination.py
+++ apps/api_set_v2/views/new_product_pagination.py
@@ -376,9 +376,9 @@ class ProductListAPIView(GenericAPIView):
                     min_price_index = 0
                     for variant in product_data[product]['variants']:
                         print(variant['price'])
-                        # if variant['price'] < min_price_val:
-                        #     min_price_val = variant['price']
-                        #     min_price_index = ind
+                        if variant['price']['effective_price'] < min_price_val:
+                            min_price_val = variant['price']
+                            min_price_index = ind
                         ind += 1
                     product_data[product]['variants'][min_price_index]['is_selected'] = True
         return product_data.values()
--- apps/api_set_v2/views/product_listing_query_based.py
+++ apps/api_set_v2/views/product_listing_query_based.py
@@ -566,9 +566,9 @@ class ProductListAPIView(GenericAPIView):
                     min_price_val = float('inf')
                     min_price_index = 0
                     for variant in product_data[product]['variants']:
-                        # if variant['price'] < min_price_val:
-                        #     min_price_val = variant['price']
-                        #     min_price_index = ind
+                        if variant['price']['effective_price'] < min_price_val:
+                            min_price_val = variant['price']
+                            min_price_index = ind
                         ind += 1
                     product_data[product]['variants'][min_price_index]['is_selected'] = True
         return product_data.values()
